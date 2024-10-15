d60cfff763a06a9da33c76453a0fe6852db4e66f
mahmoudgribej1 <mahmoud.gribej@esprit.tn>
2024-10-15 04:18:26 +0100

    Added logging to Etudiant class

--- src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
+++ src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
@@ -3,6 +3,8 @@ package tn.esprit.tpfoyer.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
 import java.util.Set;
@@ -11,12 +13,12 @@ import java.util.Set;
 @Getter
 @Setter
 @AllArgsConstructor
-
-
 @NoArgsConstructor
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Etudiant {
+
+    private static final Logger logger = LogManager.getLogger(Etudiant.class); // Logger instance
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +32,18 @@ public class Etudiant {
     @ManyToMany(mappedBy = "etudiants")
     Set<Reservation> reservations;
 
+    // Log creation of Etudiant instance
+    @PostPersist
+    public void logCreation() {
+        logger.info("Created Etudiant: {}", this);
+    }
+
+    // Log when Etudiant instance is loaded from the database
+    @PostLoad
+    public void logLoad() {
+        logger.info("Loaded Etudiant: {}", this);
+    }
+
+    // You can add additional methods for logging other actions if necessary
 }
-
-
 
